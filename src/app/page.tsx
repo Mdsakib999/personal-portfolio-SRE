00ff147ee01f85da5124ec8612db72ad45a17fed
unknown <sadikatanisha@gmail.com>
2025-08-13 16:44:43 +0600

    Add: banner & style fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import About from "@/components/Home/About";
+import Banner from "@/components/Home/Banner";
 
 
 export default function Home() {
   return (
     <div className="">
-
+      <Banner/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/Home/Banner.tsx
@@ -0,0 +1,39 @@
+import { GridPattern } from "../magicui/grid-pattern";
+ 
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+import { ArrowUp, ArrowUpRight } from "lucide-react";
+export default function Banner() {
+    return (
+  <div className="relative h-screen  flex size-full items-center justify-center overflow-hidden rounded-lg bg-background p-20">
+    <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center">
+           <h1 className="text-7xl font-semibold">Shahriar Rahman</h1>
+    <p className="text-xl text-muted-foreground m-4">Founder & CEO</p> 
+  <Button
+      variant="outline"
+      className="rounded-full px-1 pl-4 py-2 flex items-center gap-4 group"
+    >
+      <span>Explore My Ventures</span>
+      <span
+        aria-hidden
+        className="inline-flex items-center justify-center w-7 h-7 rounded-full border border-slate-700 bg-slate-900 text-white transition-transform duration-200 group-hover:scale-105"
+      >
+        <ArrowUpRight className="w-4 h-4" />
+      </span>
+    </Button>
+    </div>
+
+
+      <GridPattern
+        width={60}
+        height={60}
+        x={-1}
+        y={-1}
+        strokeDasharray={"4 2"}
+        className={cn(
+          "[mask-image:radial-gradient(800px_circle_at_center,white,transparent)]",
+        )}
+      />
+    </div>
+   )
+}
